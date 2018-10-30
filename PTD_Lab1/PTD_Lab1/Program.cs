96cd7b72513facbf6cc7f606cbe3a32130106a10
a-Zszywacz <Teresher935@GMail.com>
2018-10-30 15:36:20 +0100

    added class Zad1 with content for zad1

--- PTD_Lab1/PTD_Lab1/Program.cs
+++ PTD_Lab1/PTD_Lab1/Program.cs
@@ -6,10 +6,43 @@ using System.Threading.Tasks;
 
 namespace PTD_Lab1
 {
+
+	class Zad1 {
+		float f, phi, fs, time;
+		int n;
+		float[] x;
+
+		Zad1(float f, float phi, float fs, float time, int n)
+		{
+			this.f = f;
+			this.phi = phi;
+			this.fs = fs;
+			this.time = time;
+			this.n = n;
+			x = new float[n];
+
+			for(int i= 0; i < n; i++)
+			{
+
+			}
+
+		}
+
+		float xn(float f, int n, float fs, float phi)
+		{
+			float wynik = (float)(0.7 * Math.Sin(2 * Math.PI * (double)f * (double)n / (double)fs + (double)phi) * n);
+			return 1; //TODO: change
+		}
+
+
+	}
+
 	class Program
 	{
 		static void Main(string[] args)
 		{
+			
+			
 		}
 	}
 }
