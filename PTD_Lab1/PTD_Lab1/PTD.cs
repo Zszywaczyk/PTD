d364901c2fdbdc2f0ad8a34685a8d29fd77e67da
a-Zszywacz <Teresher935@GMail.com>
2018-11-16 06:46:26 +0100

    Long work, almost done. Ex 1, 2, 3 done. Now time for Ex.4. Graphic App Ready. Saving to img ready. Redundancy problem.

--- PTD_Lab1/PTD_Lab1/PTD.cs
+++ PTD_Lab1/PTD_Lab1/PTD.cs
@@ -9,22 +9,89 @@ namespace PTD_Lab1
 	public class PTD
 	{
 
-		float f, phi, fs, time;
-		int n;
+		float f, phi, fs, T;
+		int N;
+		float[] t;
 		float[] x;
 
-		public PTD(float f, float phi, float fs, float time, int n)
+		float[] y;
+		float[] z;
+		float[] v;
+
+		float[] u;
+
+		public PTD(float f, float phi, float fs, float T, int N)
 		{
 			this.f = f;
 			this.phi = phi;
 			this.fs = fs;
-			this.time = time;
-			this.n = n;
-			x = new float[n];
+			this.T = T;
+			this.N = N;
+			x = new float[N];
+			t = new float[N];
 
-			for (int i = 0; i < n; i++)
+			for (int i = 0; i < N; i++)
 			{
 				x[i] = xn(this.f, i, this.fs, this.phi);
+				t[i] = i * (1 / fs);
+			}
+
+			zad2();
+
+		}
+
+		public PTD(float T, float fs, int N)
+		{
+			this.T = T;
+			this.fs = fs;
+			this.N = N;
+			this.t = new float[N];
+			this.u = new float[N];
+
+			for (int i = 0; i < N; i++)
+			{
+				this.t[i] = i * (1 / fs);
+
+				if(this.t[i]>=0 && this.t[i] < 0.2)
+				{
+					this.u[i] = 0.8f * (float)Math.Sin(20 * (float)Math.PI * this.t[i]);
+				}
+				else if(this.t[i] >= 0.2 && this.t[i] < 0.4)
+				{
+					this.u[i] = (float)(Math.Exp(this.t[i] - 0.2) * 0.8 * Math.Sin(20 * Math.PI * this.t[i]));
+				}
+				else if (this.t[i] >= 0.4 && this.t[i] < 0.6)
+				{
+					this.u[i] = (float)(0.6 * Math.Sin(10 * Math.PI * this.t[i]));
+				}
+				else if (this.t[i] >= 0.6 && this.t[i] < 0.8)
+				{
+					this.u[i] = (float)(Math.Exp(this.t[i] - 0.6) * 0.6 * Math.Sin(10 * Math.PI * this.t[i]));
+				}
+				else if (this.t[i] >= 0.8 && this.t[i] < 1)
+				{
+					this.u[i] = (float)(Math.Log((0.7 * this.t[i]), 2) * 0.5 * Math.Sin(40 * Math.PI * this.t[i]));
+				}
+			}
+
+		}
+
+		private void zad2()
+		{
+			this.y = new float[this.N];
+			for(int i = 0; i < this.N; i++)
+			{
+				this.y[i] = (this.x[i] + 1) / (this.x[i] + 10);
+			}
+			this.z = new float[this.N];
+			for (int i = 0; i < this.N; i++)
+			{
+				this.z[i] = y[i] * (float)(Math.Pow((double)Math.Abs(x[i]),0.333));
+			}
+			this.v = new float[this.N];
+			for (int i = 0; i < this.N; i++)
+			{
+				v[i]=(3*x[i])+(y[i]*(Math.Abs(x[i]) + 1.78f));
 			}
 
 		}
@@ -32,14 +99,50 @@ namespace PTD_Lab1
 		float xn(float f, int n, float fs, float phi)
 		{
 			float wynik = (float)(0.7 * Math.Sin(2 * Math.PI * (double)f * (double)n / (double)fs + (double)phi) * n);
-			return wynik; //TODO: change
+			return wynik;
 		}
-		public void getXN()
+		public void printXN()
 		{
-			for (int i = 0; i < n; i++)
+			for (int i = 0; i < N; i++)
 			{
-				Console.WriteLine(i + 1 + ": " + x[i]);
+				Console.WriteLine(this.t[i] + ": " + this.x[i]);
 			}
+		}
+		public int getN()
+		{
+			return this.N;
+		}
+		public float getXN(int i)
+		{
+			return this.x[i];
+		}
+		public float getYN(int i)
+		{
+			return this.y[i];
+		}
+		public float getZN(int i)
+		{
+			return this.z[i];
+		}
+		public float getVN(int i)
+		{
+			return this.v[i];
+		}
+		public float getUN(int i)
+		{
+			return this.u[i];
+		}
+		public float getSmall_t(int i)
+		{
+			return this.t[i];
+		}
+		public float[] getX()
+		{
+			return this.x;
+		}
+		public float getBig_T()
+		{
+			return this.T;
 		}
 
 
--- PTD_Lab1/PTD_Lab1/Program.cs
+++ PTD_Lab1/PTD_Lab1/Program.cs
@@ -13,33 +13,238 @@ namespace PTD_Lab1
 
 	public partial class Program : Form
 	{
+
+		private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+
 		static void Main(string[] args)
 		{
 			//Console operations
-			float f = 4;
+			float f = 4;	//czestotliwosc Hz
 			float phi = (float)((7 * Math.PI)/9);
-			float fs = 100;
-			float time = 1;
-			int n = 100;
-			PTD zad1 = new PTD(f, phi, fs, time, n);
-			zad1.getXN();
+			float fs = 100;	//czestotliwosc probkowania
+			float T = 1;    //czas trwania sygnalu
+			int N = (int)(fs * T);	
+			PTD zad1 = new PTD(f, phi, fs, T, N);
+			zad1.printXN();
 			//========
 
 			//Run Graphic
-			Application.Run(new Program());
+			Application.Run(new Program(zad1, "1"));
+			Application.Run(new Program(zad1, "2a"));
+			Application.Run(new Program(zad1, "2b"));
+			
+
+			T = 3;    //czas trwania sygnalu
+			fs = 1200; //czestotliwosc probkowania
+			N = (int)(fs * T);
+
+			PTD zad3 = new PTD(T, fs, N);
+			Application.Run(new Program(zad3, "3"));
+
+			T = 4;
+			fs = 10000;
+			N = (int)(fs * T);
+
+			//PTD zad4 = new PTD(T, fs);
 		}
 
-		private void InitializeComponent()
+		public Program(PTD zad1, string block)
 		{
+			switch (block)
+			{
+				case "1":
+					InitializeComponent(zad1);
+					break;
+				case "2a":
+					InitializeComponent2a(zad1);
+					break;
+				case "2b":
+					InitializeComponent2b(zad1);
+					break;
+				case "3":
+					InitializeComponent3(zad1);
+					break;
+			}
+				
+		}
+
+		private void InitializeComponent(PTD zad1)
+		{
+
 			this.SuspendLayout();
+			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+			System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+			System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+			((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+			// 
+			// chart1
+			// 
+			chartArea1.Name = "ChartArea1";
+			this.chart1.ChartAreas.Add(chartArea1);
+			legend1.Name = "Legend1";
+			this.chart1.Legends.Add(legend1);
+			this.chart1.Location = new System.Drawing.Point(0, 0);
+			this.chart1.Name = "chart1";
+			series1.ChartArea = "ChartArea1";
+			series1.Legend = "Legend1";
+			series1.Name = "Series1";
+			this.chart1.Series.Add(series1);
+			this.chart1.Size = new System.Drawing.Size(1277, 526);
+			this.chart1.TabIndex = 0;
+			this.chart1.Text = "chart1";
+			this.chart1.ChartAreas[0].AxisX.Maximum = zad1.getBig_T();
+			this.chart1.ChartAreas[0].AxisX.Minimum = 0;
 			// 
 			// Program
+			this.chart1.Series[0].BorderWidth = 3;
+			for (int i = 0; i < zad1.getN(); i++)
+			{
+				this.chart1.Series[0].Points.AddXY(zad1.getSmall_t(i), zad1.getXN(i));
+				this.chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+			}
 			// 
-			this.ClientSize = new System.Drawing.Size(752, 472);
+			this.chart1.SaveImage("../../../zad1.png", System.Windows.Forms.DataVisualization.Charting.ChartImageFormat.Png); //zapisujemy charta
+			this.ClientSize = new System.Drawing.Size(1315, 587);
+			this.Controls.Add(this.chart1);
 			this.Name = "Program";
+			((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+			this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
 			this.ResumeLayout(false);
-
 		}
-	}
 
+		private void InitializeComponent2a(PTD zad1)
+		{
+
+			this.SuspendLayout();
+			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+			System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+			System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+			((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+			// 
+			// chart1
+			// 
+			chartArea1.Name = "ChartArea1";
+			this.chart1.ChartAreas.Add(chartArea1);
+			legend1.Name = "Legend1";
+			this.chart1.Legends.Add(legend1);
+			this.chart1.Location = new System.Drawing.Point(0, 0);
+			this.chart1.Name = "chart1";
+			series1.ChartArea = "ChartArea1";
+			series1.Legend = "Legend1";
+			series1.Name = "Series1";
+			this.chart1.Series.Add(series1);
+			this.chart1.Size = new System.Drawing.Size(1277, 526);
+			this.chart1.TabIndex = 0;
+			this.chart1.Text = "chart1";
+			this.chart1.ChartAreas[0].AxisX.Maximum = zad1.getBig_T();
+			this.chart1.ChartAreas[0].AxisX.Minimum = 0;
+			// 
+			// Program
+			this.chart1.Series[0].BorderWidth = 3;
+			for (int i = 0; i < zad1.getN(); i++)
+			{
+				this.chart1.Series[0].Points.AddXY(zad1.getSmall_t(i), zad1.getZN(i));
+				this.chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+			}
+			// 
+			this.chart1.SaveImage("../../../zad2a.png", System.Windows.Forms.DataVisualization.Charting.ChartImageFormat.Png); //zapisujemy charta
+			this.ClientSize = new System.Drawing.Size(1315, 587);
+			this.Controls.Add(this.chart1);
+			this.Name = "Program";
+			((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+			this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+			this.ResumeLayout(false);
+		}
+		private void InitializeComponent2b(PTD zad1)
+		{
+
+			this.SuspendLayout();
+			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+			System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+			System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+			((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+			// 
+			// chart1
+			// 
+			chartArea1.Name = "ChartArea1";
+			this.chart1.ChartAreas.Add(chartArea1);
+			legend1.Name = "Legend1";
+			this.chart1.Legends.Add(legend1);
+			this.chart1.Location = new System.Drawing.Point(0, 0);
+			this.chart1.Name = "chart1";
+			series1.ChartArea = "ChartArea1";
+			series1.Legend = "Legend1";
+			series1.Name = "Series1";
+			this.chart1.Series.Add(series1);
+			this.chart1.Size = new System.Drawing.Size(1277, 526);
+			this.chart1.TabIndex = 0;
+			this.chart1.Text = "chart1";
+			this.chart1.ChartAreas[0].AxisX.Maximum = zad1.getBig_T();
+			this.chart1.ChartAreas[0].AxisX.Minimum = 0;
+			// 
+			// Program
+			this.chart1.Series[0].BorderWidth = 3;
+			for (int i = 0; i < zad1.getN(); i++)
+			{
+				this.chart1.Series[0].Points.AddXY(zad1.getSmall_t(i), zad1.getVN(i));
+				this.chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+			}
+			// 
+			this.chart1.SaveImage("../../../zad2b.png", System.Windows.Forms.DataVisualization.Charting.ChartImageFormat.Png); //zapisujemy charta
+			this.ClientSize = new System.Drawing.Size(1315, 587);
+			this.Controls.Add(this.chart1);
+			this.Name = "Program";
+			((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+			this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+			this.ResumeLayout(false);
+		}
+		private void InitializeComponent3(PTD zad1)
+		{
+
+			this.SuspendLayout();
+			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+			System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+			System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+			((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+			// 
+			// chart1
+			// 
+			chartArea1.Name = "ChartArea1";
+			this.chart1.ChartAreas.Add(chartArea1);
+			legend1.Name = "Legend1";
+			this.chart1.Legends.Add(legend1);
+			this.chart1.Location = new System.Drawing.Point(0, 0);
+			this.chart1.Name = "chart1";
+			series1.ChartArea = "ChartArea1";
+			series1.Legend = "Legend1";
+			series1.Name = "Series1";
+			this.chart1.Series.Add(series1);
+			this.chart1.Size = new System.Drawing.Size(1277, 526);
+			this.chart1.TabIndex = 0;
+			this.chart1.Text = "chart1";
+			this.chart1.ChartAreas[0].AxisX.Maximum = zad1.getBig_T();
+			this.chart1.ChartAreas[0].AxisX.Minimum = 0;
+			// 
+			// Program
+			this.chart1.Series[0].BorderWidth = 3;
+			for (int i = 0; i < zad1.getN(); i++)
+			{
+				this.chart1.Series[0].Points.AddXY(zad1.getSmall_t(i), zad1.getUN(i));
+				this.chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+			}
+			// 
+			this.chart1.SaveImage("../../../zad3.png", System.Windows.Forms.DataVisualization.Charting.ChartImageFormat.Png); //zapisujemy charta
+			this.ClientSize = new System.Drawing.Size(1315, 587);
+			this.Controls.Add(this.chart1);
+			this.Name = "Program";
+			((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+			this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+			this.ResumeLayout(false);
+		}
+
+	}
 }
