ea6b0c34ea26b7abc9eccd0b9ca5849b561de03d
a-Zszywacz <Teresher935@GMail.com>
2018-12-30 16:09:51 +0100

    Lab3 done

---
+++ PTD_Lab3/PTD_Lab3/PTD.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using FFTLibrary;
+
+namespace PTD_Lab3
+{
+	public class PTD
+	{
+		double[] m; //sygnał modulujący; ton prosty
+		double[] zA; //sygnał zmodulowany amplitudy
+		double[] zP; //sygnał zmodulowany kąta
+		double kA; //wspolczynnik glebokosci modulacji
+		double kP;
+		double fn; //czestotliwosc sygnalu nośnego
+		double fm; //czestotliwosc sygnalu informacyjnegoS
+		double fs;
+		double Am; //amplituda sygnału modulującego
+		double T; //czas całkowity
+		double[] t; //czas dla próbek
+		double N; //liczba próbek
+		double[][] widmo_zA;
+		double[] widmo_zA_log;
+		double[] widmo_zA_fK;
+		double[][] widmo_zP;
+		double[] widmo_zP_log;
+		double[] widmo_zP_fK;
+		double m3A, m3P;
+		double[] max = new double[3];
+		double[] max_pos = new double[3];
+
+		int choice;
+
+		public PTD(double fs, double fm, double fn, double kA, double kP, double T, double Am, int choice)
+		{
+			this.fs = fs; this.fm = fm; this.fn = fn;  this.T = T; this.Am = Am; this.choice = choice;
+
+			this.N = fs * T;
+			this.m = new double[(int)N]; this.t = new double[(int)N];
+			mt(); //liczymy m(t)
+
+			this.kA = kA;
+			this.zA = new double[(int)N]; this.t = new double[(int)N];
+			zAt(); //liczymy zA(t)
+			widmo_zA = operationFFT(zA);
+			widmo_zA_fK = widmo_zA[1];
+			widmo_zA_log = operationLog(widmo_zA[0]);
+			m3A = findM3(widmo_zA_log);
+			findMaxDb(widmo_zA_log);
+
+			this.kP = kP;
+			this.zP = new double[(int)N]; this.t = new double[(int)N];
+			zPt(); //liczymy zP(t)
+			widmo_zP = operationFFT(zP);
+			widmo_zP_fK = widmo_zP[1];
+			widmo_zP_log = operationLog(widmo_zP[0]);
+			//m3P = findM3(widmo_zP_log);
+
+			//widma
+		}
+		private void mt()
+		{
+			for (int i=0;i<(int)N;i++)
+			{
+				//this.t[i] = new double();
+				this.t[i] = i / fs;
+				m[i] = Am * Math.Sin(2 * Math.PI * fm * t[i]);
+				//Console.WriteLine("{0}: {1}", i, t[i]);
+				//Console.WriteLine("{0} * Sin(2 * {1} * {2} * {3})", Am, Math.PI, fm, t[i]);
+			}
+			//Console.WriteLine(Am);
+		}
+		private void zAt()
+		{
+			for (int i = 0; i < (int)N; i++)
+			{
+				this.t[i] = i / fs;
+				zA[i] = (kA * m[i] + 1) * Math.Cos(2 * Math.PI * fn * t[i]);
+				//Console.WriteLine("{0}: {1}", i, zA[i]);
+			}
+		}
+		private void zPt()
+		{
+			for (int i = 0; i < (int)N; i++)
+			{
+				this.t[i] = i / fs;
+				zP[i] = Math.Cos(2 * Math.PI * fn * t[i] + kP * m[i]);
+				//Console.WriteLine("{0}: {1}", i, zA[i]);
+			}
+		}
+		private double[][] operationFFT(double[] x)
+		{
+
+			double fftN = 0;
+			int m = 0;
+			while((fftN = Math.Pow(2, m)) < N)
+			{
+				m++;
+			}
+			int localN = (int)fftN / 2;
+			double[] im = new double[(int)fftN];
+			double[] re = new double[(int)fftN];
+			double[][] result = new double[2][];
+			for (int i = 0; i < fftN; i++)
+			{
+				im[i] = 0;
+				re[i] = x[i];
+				
+			}
+			FFTLibrary.Complex.FFT(1, m, re, im);
+			result[0] = new double[localN];
+			result[1] = new double[localN];
+			for (int i = 0; i < localN; i++)
+			{
+				result[0][i] = Math.Sqrt(re[i] * re[i] + im[i] * im[i]);
+				result[1][i] = i * (fs / fftN);
+			}
+
+				return result;
+			
+		}
+		private double[] operationLog(double[] localX)
+		{
+			double[] localY = new double[localX.Length];
+			for(int i = 0; i < localX.Length; i++)
+			{
+				localY[i] = 10 * Math.Log(Math.Abs(localX[i]));
+				//Console.WriteLine(i+": "+localX[i]);
+			}
+			//findMaxDb(localY);
+			return localY;
+		}
+		private void findMaxDb(double[] localY)
+		{
+			max[0] = localY[0];
+			for(int i = 1; i<localY.Length; i++)
+			{
+				if(max[0] < localY[i])
+				{
+					max[0] = localY[i];
+					max_pos[0] = i;
+				}
+			}
+			//Console.WriteLine(max[0]);
+			max[1] = max[0]-40000;
+			for (int i = 1; i < localY.Length; i++)
+			{
+				if (max[1] < localY[i] && localY[i] < max[0])
+				{
+					max[1] = localY[i];
+					max_pos[1] = i;
+				}
+			}
+			//Console.WriteLine(max[1]);
+			max[2] = max[1] - 40000;
+			for (int i = 1; i < localY.Length; i++)
+			{
+				if (max[2] < localY[i] && localY[i] < max[1])
+				{
+					max[2] = localY[i];
+					max_pos[2] = i;
+				}
+			}
+			//Console.WriteLine(max[2]);
+			//Console.WriteLine();
+			double temp;
+			double temp_pos;
+
+			for(int i = 1; i < 3; i++)
+			{
+				for (int j = i; j < 3; j++)
+				{
+					if (max_pos[i - 1] > max_pos[j])
+					{
+						temp_pos = max_pos[j];
+						temp = max[j];
+
+						max_pos[j] = max_pos[i - 1];
+						max[j] = max[i - 1];
+
+						max[i - 1] = temp;
+						max_pos[i - 1] = temp_pos;
+					}
+				}
+			}
+			Console.WriteLine(max[0]);
+			Console.WriteLine(max[1]);
+			Console.WriteLine(max[2]);
+
+			Console.WriteLine();
+		}
+
+		private double findM3(double[] localY)
+		{
+			double max = localY[0];
+			for (int i = 1; i < localY.Length; i++)
+			{
+				if (max < localY[i])
+				{
+					max = localY[i];
+				}
+			}
+			return max-3;
+		}
+
+		public static void SaveToFile()
+		{
+
+		}
+
+		public double[] get_t { get => t;}
+		public double get_T { get => T; }
+		public double[] get_m { get => m; }
+		public int getN { get => (int)N; }
+		public double[] get_zA { get => zA; }
+		public double[] get_zP { get => zP; }
+		public int getsetChoice { get => choice; set => choice = value; }
+		public double[][] Widmo_zP { get => widmo_zP; }
+		public double[] Widmo_zP_log { get => widmo_zP_log; }
+		public double[] Widmo_zP_fK { get => widmo_zP_fK; }
+		public double[][] Widmo_zA { get => widmo_zA; }
+		public double[] Widmo_zA_log { get => widmo_zA_log; }
+		public double[] Widmo_zA_fK { get => widmo_zA_fK; }
+		public double getM3A { get => m3A;}
+		public double getM3P { get => m3P;}
+		public double[] getMax { get => max; }
+		public double[] getMax_pos { get => max_pos; }
+		public double getKA { get => kA; }
+		public double getKP{ get => kP; }
+	}
+}
---
+++ PTD_Lab3/PTD_Lab3/Program.cs
@@ -0,0 +1,373 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PTD_Lab3
+{
+	public partial class Program : Form
+	{
+
+		public static int iii;
+		private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+
+		static void Main(string[] args)
+		{
+			iii = 1;
+			{/*
+			double fn=10000;//znacznie wieksze od fm
+			double fs = 512;
+			double fm = 1;
+			double T = 1;
+			double Am = 1;
+			double kA = 0.8;
+			double kP = 0.5; //wspolczynnik glebokosci modulacji (w %)
+			//PTD ptd1 = new PTD(fs, fm, fn, kA, kP, T, Am, 4);
+			//Application.Run(new Program(ptd1));
+			PTD ptd1 = new PTD(fs, fm, fn, kA, kP, T, Am, 2);
+			Application.Run(new Program(ptd1));
+			Application.Run(new Program(ptd1)); // tu choice = 6
+			*/
+			}
+			/*	double fn = 250;//znacznie wieksze od fm
+			double fs = 512; // fs >= 2*fn
+			double fm = 2;
+			double T = 1;
+			double Am = 1;
+			double kA;
+			double kP;*/
+			double fn = 10000; //znacznie wieksze od fm
+			double fs = 32768; // fs >= 2*fn
+			double fm = 6; //znacznie mniejsze od fn
+			double T = 0.5;
+			double Am = 2;
+			double kA;
+			double kP;
+
+			kA = 0.8;
+			kP = 0.5;
+			PTD ptd1 = new PTD(fs, fm, fn, kA, kP, T, Am, 111);
+			Application.Run(new Program(ptd1));
+			Application.Run(new Program(ptd1));
+			Application.Run(new Program(ptd1));
+			Application.Run(new Program(ptd1));
+
+			kA = 10;
+			kP = 2.14;
+			PTD ptd2 = new PTD(fs, fm, fn, kA, kP, T, Am, 333);
+			Application.Run(new Program(ptd2));
+			Application.Run(new Program(ptd2));
+			Application.Run(new Program(ptd2));
+			Application.Run(new Program(ptd2));
+
+			kA = 34;
+			kP = 66;
+			PTD ptd3 = new PTD(fs, fm, fn, kA, kP, T, Am, 555);
+			Application.Run(new Program(ptd3));
+			Application.Run(new Program(ptd3));
+			Application.Run(new Program(ptd3));
+			Application.Run(new Program(ptd3));
+
+			Console.ReadKey();
+		}
+		public Program(PTD ptd)
+		{
+					InitializeComponent(ptd);
+		}
+		private void InitializeComponent(PTD ptd)
+		{
+			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+			System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+			System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+			((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// chart1
+			// 
+			chartArea1.Name = "ChartArea1";
+			this.chart1.ChartAreas.Add(chartArea1);
+			legend1.Name = "Legend1";
+			this.chart1.Legends.Add(legend1);
+			this.chart1.Location = new System.Drawing.Point(12, 12);
+			this.chart1.Name = "chart1";
+			series1.ChartArea = "ChartArea1";
+			series1.Legend = "Legend1";
+			
+			this.chart1.Series.Add(series1);
+			this.chart1.Size = new System.Drawing.Size(1205, 563);
+			this.chart1.TabIndex = 0;
+			this.chart1.Text = "chart1";
+			this.chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+			
+			
+			/*this.chart1.ChartAreas[0].AxisX.Minimum = 230;
+			this.chart1.ChartAreas[0].AxisX.Maximum = 250;
+			this.chart1.ChartAreas[0].AxisY.Minimum = -11;
+			this.chart1.ChartAreas[0].AxisY.Maximum = -6.5;*/
+			// 
+			// Program
+			switch (ptd.getsetChoice)
+			{
+				default:
+					break;
+				/*
+				case 1:
+					this.chart1.ChartAreas[0].AxisX.Maximum = ptd.get_T;
+					this.chart1.Series[0].LegendText = "m(t)";
+					for (int i = 0; i < ptd.getN; i++)	{	this.chart1.Series[0].Points.AddXY(ptd.get_t[i], ptd.get_m[i]); }
+					break;
+				case 2:
+					this.chart1.ChartAreas[0].AxisX.Maximum = ptd.get_T;
+					System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+					this.chart1.Series.Add(series2);
+					this.chart1.Series[1].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+					this.chart1.Series[0].LegendText = "m(t)";
+					this.chart1.Series[1].LegendText = "zA(t)";
+					for (int i = 0; i < ptd.getN; i++) { this.chart1.Series[0].Points.AddXY(ptd.get_t[i], ptd.get_m[i]); this.chart1.Series[1].Points.AddXY(ptd.get_t[i], ptd.get_zA[i]);  }
+					break;
+				case 3:
+					this.chart1.ChartAreas[0].AxisX.Maximum = ptd.get_T;
+					System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+					this.chart1.Series.Add(series3);
+					this.chart1.Series[1].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+					this.chart1.Series[0].LegendText = "m(t)";
+					this.chart1.Series[1].LegendText = "zP(t)";
+					for (int i = 0; i < ptd.getN; i++) { this.chart1.Series[0].Points.AddXY(ptd.get_t[i], ptd.get_m[i]); this.chart1.Series[1].Points.AddXY(ptd.get_t[i], ptd.get_zP[i]); }
+					break;
+				case 4:
+					this.chart1.ChartAreas[0].AxisX.Maximum = ptd.get_T;
+					System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+					System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+					this.chart1.Series.Add(series4);
+					this.chart1.Series.Add(series5);
+					this.chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+					this.chart1.Series[1].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+					this.chart1.Series[2].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+					this.chart1.Series[2].Color = System.Drawing.Color.Violet;
+					this.chart1.Series[0].LegendText = "m(t)";
+					this.chart1.Series[1].LegendText = "zA(t)";
+					this.chart1.Series[2].LegendText = "zP(t)";
+					this.chart1.Series[2].BorderWidth = 2;
+					for (int i = 0; i < ptd.getN; i++) { this.chart1.Series[0].Points.AddXY(ptd.get_t[i], ptd.get_m[i]); this.chart1.Series[1].Points.AddXY(ptd.get_t[i], ptd.get_zA[i]); this.chart1.Series[2].Points.AddXY(ptd.get_t[i], ptd.get_zP[i]); }
+					break;
+				case 5:
+					this.chart1.Series[0].BorderWidth = 3;
+					for (int i = 0; i < ptd.getN / 2; i++)
+					{
+						this.chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+						this.chart1.Series[0].Points.AddXY(ptd.Widmo_zA_fK[i], ptd.Widmo_zA_log[i]);
+						//Console.WriteLine(ptd.Widmo_zA[i].im);
+					}
+					ptd.getsetChoice = 6;
+					break;
+				case 6:
+					this.chart1.Series[0].BorderWidth = 3;
+					for (int i = 0; i < ptd.getN / 2; i++)
+					{
+						this.chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+						this.chart1.Series[0].Points.AddXY(ptd.Widmo_zP_fK[i], ptd.Widmo_zP_log[i]);
+						//Console.WriteLine(ptd.Widmo_zA[i].im);
+					}
+					
+					break;
+				//===================================
+				//Testing testing...
+				case 55:
+					this.chart1.Series[0].BorderWidth = 3;
+					for (int i = 0; i < ptd.getN / 2; i++)
+					{
+						this.chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+						this.chart1.Series[0].Points.AddXY(ptd.Widmo_zA[1][i], ptd.Widmo_zA[0][i]);
+						//Console.WriteLine(ptd.Widmo_zA[i].im);
+					}
+					ptd.getsetChoice = 66;
+					break;
+				case 66:
+					this.chart1.Series[0].BorderWidth = 3;
+					for (int i = 0; i < ptd.getN / 2; i++)
+					{
+						this.chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+						this.chart1.Series[0].Points.AddXY(ptd.Widmo_zP[1][i], ptd.Widmo_zP[0][i]);
+						//Console.WriteLine(ptd.Widmo_zA[i].im);
+					}
+					ptd.getsetChoice = 6;
+					break;
+				//===================================
+				*/
+			}
+			//this.chart1.ChartAreas[0].AxisX.Minimum = 247;
+			//this.chart1.ChartAreas[0].AxisX.Maximum = 255;
+
+			System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			this.chart1.Series.Add(series2);
+			this.chart1.Series[1].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+			this.chart1.Series[1].Color = System.Drawing.Color.Red;
+			this.chart1.Series[0].BorderWidth = 1;
+			this.chart1.ChartAreas[0].AxisX.Minimum = 0;
+			//this.chart1.ChartAreas[0].AxisX.Maximum = (ptd.getN/2)-1;
+
+			switch (ptd.getsetChoice)
+			{
+				case 111:
+					
+					for (int i = 0; i < (ptd.getN / 2); i++)
+					{
+						this.chart1.Series[0].Points.AddXY(ptd.Widmo_zA[1][i], ptd.Widmo_zA[0][i]);
+						ptd.getsetChoice = 222;
+					}
+					break;
+				case 222:
+					for (int i = 0; i < (ptd.getN / 2); i++)
+					{
+						this.chart1.Series[0].Points.AddXY(ptd.Widmo_zP[1][i], ptd.Widmo_zP[0][i]);
+					}
+					ptd.getsetChoice = -111;
+					break;
+				case 333:
+					for (int i = 0; i < (ptd.getN / 2); i++)
+					{
+						this.chart1.Series[0].Points.AddXY(ptd.Widmo_zA[1][i], ptd.Widmo_zA[0][i]);
+					}
+					ptd.getsetChoice = 444;
+					break;
+				case 444:
+					for (int i = 0; i < (ptd.getN / 2); i++)
+					{
+						this.chart1.Series[0].Points.AddXY(ptd.Widmo_zP[1][i], ptd.Widmo_zP[0][i]);
+					}
+					ptd.getsetChoice = -333;
+					break;
+				case 555:
+					for (int i = 0; i < (ptd.getN / 2); i++)
+					{
+						this.chart1.Series[0].Points.AddXY(ptd.Widmo_zA[1][i], ptd.Widmo_zA[0][i]);
+					}
+					ptd.getsetChoice = 666;
+					break;
+				case 666:
+					for (int i = 0; i < (ptd.getN / 2); i++)
+					{
+						this.chart1.Series[0].Points.AddXY(ptd.Widmo_zP[1][i], ptd.Widmo_zP[0][i]);
+					}
+					ptd.getsetChoice = -555;
+					break;
+
+
+				case -111:
+					series1.Name = iii+". Mod amp\n" + "kA="+ptd.getKA+"\n";
+					this.Text = iii + ". Mod amp  " + "kA=" + ptd.getKA + "\n";
+					iii++;
+					for (int i = 1; i < ptd.Widmo_zA_log.Length; i++)
+					{
+						this.chart1.Series[0].Points.AddXY(ptd.Widmo_zA_fK[i], ptd.Widmo_zA_log[i]);
+						//this.chart1.Series[1].Points.AddXY(ptd.Widmo_zA_fK[i], ptd.getM3A);
+						
+						if(ptd.getMax[0]>= ptd.getMax[1]-3 && ptd.getMax[2] >= ptd.getMax[1]-3)
+						{
+							if(ptd.getMax_pos[0] == i)
+							{
+								this.chart1.Series[1].Points.AddXY(ptd.Widmo_zA_fK[i], ptd.getMax[1] - 3);
+							}
+							if (ptd.getMax_pos[1] == i)
+							{
+								this.chart1.Series[1].Points.AddXY(ptd.Widmo_zA_fK[i], ptd.getMax[1] - 3);
+							}
+							if (ptd.getMax_pos[2] == i)
+							{
+								this.chart1.Series[1].Points.AddXY(ptd.Widmo_zA_fK[i], ptd.getMax[1] - 3);
+							}
+						}
+					}
+					Console.WriteLine("Szerokość pasma: "+ (ptd.Widmo_zA_fK[(int)ptd.getMax_pos[2]] - ptd.Widmo_zA_fK[(int)ptd.getMax_pos[0]]) + " Hz");
+					//this.chart1.ChartAreas[0].AxisX.Minimum = 10000-100;
+					//this.chart1.ChartAreas[0].AxisX.Maximum = 10000+100;
+					ptd.getsetChoice = -222;
+					this.chart1.SaveImage("../../../"+(iii-1)+" - zA.png", System.Windows.Forms.DataVisualization.Charting.ChartImageFormat.Png); //zapisujemy charta
+					break;
+				case -222:
+					series1.Name = iii + ". Mod kat\n" + "kP=" + ptd.getKP + "\n";
+					this.Text = iii + ". Mod kat  " + "kP=" + ptd.getKP + "\n";
+					iii++;
+					for (int i = 1; i < ptd.Widmo_zP_log.Length; i++)
+					{
+						this.chart1.Series[0].Points.AddXY(ptd.Widmo_zP_fK[i], ptd.Widmo_zP_log[i]);
+						//this.chart1.Series[1].Points.AddXY(ptd.Widmo_zP_fK[i], ptd.getM3P);
+					}
+					//this.chart1.ChartAreas[0].AxisX.Minimum = 10000 - 100;
+					//this.chart1.ChartAreas[0].AxisX.Maximum = 10000 + 100;
+					this.chart1.SaveImage("../../../" + (iii - 1) + " - zP.png", System.Windows.Forms.DataVisualization.Charting.ChartImageFormat.Png); //zapisujemy charta
+					break;
+				case -333:
+					series1.Name = iii + ". Mod amp\n" + "kA=" + ptd.getKA + "\n";
+					this.Text = iii + ". Mod amp  " + "kA=" + ptd.getKA + "\n";
+					iii++;
+					for (int i = 1; i < ptd.Widmo_zA_log.Length; i++)
+					{
+						this.chart1.Series[0].Points.AddXY(ptd.Widmo_zA_fK[i], ptd.Widmo_zA_log[i]);
+						//this.chart1.Series[1].Points.AddXY(ptd.Widmo_zA_fK[i], ptd.getM3A);
+					}
+					//this.chart1.ChartAreas[0].AxisX.Minimum = 10000 - 100;
+					//this.chart1.ChartAreas[0].AxisX.Maximum = 10000 + 100;
+					ptd.getsetChoice = -444;
+					this.chart1.SaveImage("../../../" + (iii - 1) + " - zA.png", System.Windows.Forms.DataVisualization.Charting.ChartImageFormat.Png); //zapisujemy charta
+					break;
+				case -444:
+					series1.Name = iii + ". Mod kat\n" + "kP=" + ptd.getKP + "\n";
+					this.Text = iii + ". Mod kat  " + "kP=" + ptd.getKP + "\n";
+					iii++;
+					for (int i = 1; i < ptd.Widmo_zP_log.Length; i++)
+					{
+						this.chart1.Series[0].Points.AddXY(ptd.Widmo_zP_fK[i], ptd.Widmo_zP_log[i]);
+						//this.chart1.Series[1].Points.AddXY(ptd.Widmo_zP_fK[i], ptd.getM3P);
+					}
+					//this.chart1.ChartAreas[0].AxisX.Minimum = 10000 - 100;
+					//this.chart1.ChartAreas[0].AxisX.Maximum = 10000 + 100;
+					this.chart1.SaveImage("../../../" + (iii - 1) + " - zP.png", System.Windows.Forms.DataVisualization.Charting.ChartImageFormat.Png); //zapisujemy charta
+					break;
+				case -555:
+					series1.Name = iii + ". Mod amp\n" + "kA=" + ptd.getKA + "\n";
+					this.Text = iii + ". Mod amp  " + "kA=" + ptd.getKA + "\n";
+					iii++;
+					for (int i = 1; i < ptd.Widmo_zA_log.Length; i++)
+					{
+						this.chart1.Series[0].Points.AddXY(ptd.Widmo_zA_fK[i], ptd.Widmo_zA_log[i]);
+						//this.chart1.Series[1].Points.AddXY(ptd.Widmo_zA_fK[i], ptd.getM3A);
+					}
+					//this.chart1.ChartAreas[0].AxisX.Minimum = 10000 - 100;
+					//this.chart1.ChartAreas[0].AxisX.Maximum = 10000 + 100;
+					ptd.getsetChoice = -666;
+					this.chart1.SaveImage("../../../" + (iii - 1) + " - zA.png", System.Windows.Forms.DataVisualization.Charting.ChartImageFormat.Png); //zapisujemy charta
+					break;
+				case -666:
+					series1.Name = iii + ". Mod kat\n" + "kP" + ptd.getKP + "\n";
+					this.Text = iii + ". Mod kat  " + "kP" + ptd.getKP + "\n";
+					iii++;
+					for (int i = 1; i < ptd.Widmo_zP_log.Length; i++)
+					{
+						this.chart1.Series[0].Points.AddXY(ptd.Widmo_zP_fK[i], ptd.Widmo_zP_log[i]);
+						//this.chart1.Series[1].Points.AddXY(ptd.Widmo_zP_fK[i], ptd.getM3P);
+					}
+					//this.chart1.ChartAreas[0].AxisX.Minimum = 10000 - 100;
+					//this.chart1.ChartAreas[0].AxisX.Maximum = 10000 + 100;
+					this.chart1.SaveImage("../../../" + (iii - 1) + " - zP.png", System.Windows.Forms.DataVisualization.Charting.ChartImageFormat.Png); //zapisujemy charta
+					break;
+			}
+
+			this.ClientSize = new System.Drawing.Size(1315, 587);
+			this.Controls.Add(this.chart1);
+			this.Name = "Program";
+			
+			((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+
+	}
+	
+
+}
