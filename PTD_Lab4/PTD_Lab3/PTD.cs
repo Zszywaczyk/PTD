68be313be2e180c42f6c0eef37a514f7ff149bce
a-Zszywacz <Teresher935@GMail.com>
2019-01-10 13:44:52 +0100

    initial

---
+++ PTD_Lab4/PTD_Lab3/PTD.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using FFTLibrary;
+
+namespace PTD_Lab3
+{
+	public class PTD
+	{
+		string bits;
+		double[] mt;
+		double Tb;
+		int N;
+
+		int choice;
+
+		public PTD(string bits, double Tb)
+		{
+			this.bits = bits;
+			this.Tb = Tb;
+			N = bits.Length * (int)Tb;
+
+		}
+
+		public string getBits { get => bits; }
+		public double getTb { get => Tb; }
+		public int getN { get => N; set => N = value; }
+	}
+}
---
+++ PTD_Lab4/PTD_Lab3/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PTD_Lab3
+{
+	public partial class Program : Form
+	{
+
+		public static int iii;
+		private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+
+		static void Main(string[] args)
+		{
+			/*double fn = 10000; //znacznie wieksze od fm
+			double fs = 32768; // fs >= 2*fn
+			double fm = 6; //znacznie mniejsze od fn
+			double T = 0.5;
+			double Am = 2;
+			double kA;
+			double kP;*/
+			string bits = "00101";
+			double Tb = 5;
+
+			PTD ptd1 = new PTD(bits, Tb);
+			Application.Run(new Program(ptd1));
+
+			Console.ReadKey();
+		}
+		public Program(PTD ptd)
+		{
+					InitializeComponent(ptd);
+		}
+		private void InitializeComponent(PTD ptd)
+		{
+			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+			System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+			System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+			((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// chart1
+			// 
+			chartArea1.Name = "ChartArea1";
+			this.chart1.ChartAreas.Add(chartArea1);
+			legend1.Name = "Legend1";
+			this.chart1.Legends.Add(legend1);
+			this.chart1.Location = new System.Drawing.Point(12, 12);
+			this.chart1.Name = "chart1";
+			series1.ChartArea = "ChartArea1";
+			series1.Legend = "Legend1";
+			
+			this.chart1.Series.Add(series1);
+			this.chart1.Size = new System.Drawing.Size(1205, 563);
+			this.chart1.TabIndex = 0;
+			this.chart1.Text = "chart1";
+			this.chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+			this.chart1.Series[0].BorderWidth = 3;
+
+			int i = 0;
+			for (int j = 0; j < ptd.getN; j++)
+			{
+
+				if (ptd.getBits[i] == '1')
+				{
+					this.chart1.Series[0].Points.AddXY(j, 1);
+				}
+				else if (ptd.getBits[i] == '0')
+				{
+					this.chart1.Series[0].Points.AddXY(j, 0);
+				}
+
+				//if (i= j / ptd.getTb) {
+
+				//}
+				
+			}
+
+			this.ClientSize = new System.Drawing.Size(1315, 587);
+			this.Controls.Add(this.chart1);
+			this.Name = "Program";
+
+			((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+
+	}
+	
+
+}
